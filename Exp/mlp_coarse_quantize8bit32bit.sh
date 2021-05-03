c53bac46125469dceb14f6cac5d5dc8281fd49d4
Your Name <you@example.com>
2021-05-03 16:00:16 +0000

    update

--- Exp/mlp_coarse_quantize8bit32bit.sh
+++ Exp/mlp_coarse_quantize8bit32bit.sh
@@ -1,1 +1,1 @@
-nnfusion mlp_norelu_small.onnx -f onnx  -fblockfusion_level=0 -fquantize_cfg mlp_coarse_quantize_cfg
+nnfusion mlp_norelu_small.onnx -f onnx  -fblockfusion_level=0 -fquantize_cfg mlp_coarse_quantize_cfg -fdefault_device=ROCm
