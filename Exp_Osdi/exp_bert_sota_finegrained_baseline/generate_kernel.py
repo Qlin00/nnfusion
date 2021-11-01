62399bce25d58c9ea8fd6f67c1ad3b126b889e44
Ningxin <Ningxin.Zheng@microsoft.com>
2021-11-01 14:25:49 +0800

    update

---
+++ Exp_Osdi/exp_bert_sota_finegrained_baseline/generate_kernel.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import json
+from tempfile import template
+import torch
+from numpy import core
+import json
+
+import onnx
+import re
+import numpy as np
+from SparGen.Common.Utils import *
+
+if os.path.exists('/home/v-linbin/.cache/nnfusion/kernel_cache.db'):
+    os.remove('/home/v-linbin/.cache/nnfusion/kernel_cache.db')
+
+tesa_path = './tesa'
+state_path = './bert_finegrained_0.95_nomask.pth'
+id_2_maps = './tesaid_2_names'
+out_dir = './nnfusion_cfg'
+
+generate_sputnik_sparse_cfg(tesa_path, state_path, id_2_maps, out_dir)
+
+with open('../Template/sputnik_sparse_template.cu') as f:
+    code = f.read()
+with open('../Template/sputnik_sparse_template.json') as f:
+    template = json.load(f)
+    tempalte = template[0]
+
+
+id2name = torch.load('tesaid_2_names')
+with open('nnfusion_cfg/config', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = re.split(' ', line)
+        tesa_id = int(line[0])
+        kernel_id = line[2]
+        torch_name = id2name[tesa_id][0]
+        new_code = code + ' ' * tesa_id
+        tempalte['code'] = new_code
+        f_path = os.path.join(prefix, f"{tesa_id}.json")
+        print(f_path)
+        with open(f_path, 'w') as f:
+            json.dump(template, f)
+        os.system(f"python ../../src/tools/nnfusion/kernel_db/convert_external_spargen.py {f_path}")
