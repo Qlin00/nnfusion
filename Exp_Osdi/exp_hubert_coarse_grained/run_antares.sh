3d60b44ab41dba8ba8f0b901e4845ecd1f4afc1f
nizhen <nizhen>
2022-04-11 14:33:22 +0900

    update

--- Exp_Osdi/exp_hubert_coarse_grained/run_antares.sh
+++ Exp_Osdi/exp_hubert_coarse_grained/run_antares.sh
@@ -1,2 +1,2 @@
-tuning_step=20
+tuning_step=1000
 nnfusion hubert_coarse_grained.onnx -f onnx -fkernel_tuning_steps=$tuning_step -fantares_mode=1 -fantares_codegen_server="127.0.0.1:10088" -fir_based_fusion=true -fkernel_fusion_level=0 -fblockfusion_level=0 -ftuning_blocklist="Dot,QuantizeDot,InstanceNorm,Softmax" -firfusion_blocklist="Dot,QuantizeDot,InstanceNorm,Softmax"
