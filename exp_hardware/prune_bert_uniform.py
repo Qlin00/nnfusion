3a490216d4c8c91226938ed7c1292738cdb11ea1
nizhen <nizhen>
2022-05-31 22:45:34 +0900

    update

--- exp_hardware/prune_bert_uniform.py
+++ exp_hardware/prune_bert_uniform.py
@@ -119,7 +119,8 @@ if __name__ == '__main__':
     # config_list = [{'op_types': ['Linear'], 'op_names': op_names, 'sparsity': sparsity}]
     config_list = [{'op_types': ['Linear'], 'sparsity': sparsity}]
     # import ipdb; ipdb.set_trace()
-    data_dir = f"bert_{sparsity}_uniform_align_n{args.alignn}"
+    data_dir = f"bert_{sparsity}_uniform_align_n_{args.alignn}"
+    os.makedirs(data_dir, exist_ok=True)
     # pruner = HardwareAwarePruner(model, config_list, hardware_evaluator, align_n_set=[1,2,4,8,16,32], experiment_data_dir=data_dir, need_sort=False)
     pruner = BalancedPruner(model, config_list, align_n=args.alignn, balance_gran=[32])
     model, masks = pruner.compress()
