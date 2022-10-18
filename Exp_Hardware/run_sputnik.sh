f896394df27a182ec157a118cf6067b6804857e6
nizhen <nizhen>
2022-10-18 18:12:34 +0900

    update

--- Exp_Hardware/run_sputnik.sh
+++ Exp_Hardware/run_sputnik.sh
@@ -4,6 +4,8 @@ out_dir=nnfusion_cfg_test
 mkdir $out_dir
 cp ${prefix}/* $out_dir
 pushd $out_dir
+#nnfusion model_tesa.onnx -f onnx -flayernorm_fusion=1 -fgelu_fusion=1 -fspargen_cfg config -fsparse_dot_transpose=true
 nnfusion model_tesa.onnx -f onnx -flayernorm_fusion=1 -fgelu_fusion=1 -fspargen_cfg config
 popd
+cp SputnikCMakeLists.txt ${out_dir}/nnfusion_rt/cuda_codegen/CMakeLists.txt
 
