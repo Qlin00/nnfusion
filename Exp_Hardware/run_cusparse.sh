0b97a7b9ae95f7d1ce0f7d9b6cb6cc1335184833
nizhen <nizhen>
2022-10-19 14:30:52 +0900

    update

---
+++ Exp_Hardware/run_cusparse.sh
@@ -0,0 +1,11 @@
+rm -rf ~/.cache/nnfusion/kernel_cache.db
+prefix=cusparse_bert_large_n_4_m_32_align1
+out_dir=nnfusion_cfg_test
+mkdir $out_dir
+cp ${prefix}/* $out_dir
+pushd $out_dir
+#nnfusion model_tesa.onnx -f onnx -flayernorm_fusion=1 -fgelu_fusion=1 -fspargen_cfg config -fsparse_dot_transpose=true
+nnfusion model_tesa.onnx -f onnx -flayernorm_fusion=1 -fgelu_fusion=1 -fspargen_cfg config
+popd
+#cp SputnikCMakeLists.txt ${out_dir}/nnfusion_rt/cuda_codegen/CMakeLists.txt
+
